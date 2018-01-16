eab264672ad42952c89d537801a2d48f48f11178
simrose <nikito666>
2018-01-15 21:33:00 -0300

    Agregado el archivo de lista de compras con formateo de str

---
+++ lista_compras.py
@@ -0,0 +1,19 @@
+mi_lista = []
+
+elemento_lista = input("Que deseas agregar a la lista?(escriba FIN para cancelar)").upper()
+
+while elemento_lista != "FIN":
+    mi_lista.append(elemento_lista)
+    elemento_lista = input("Que deseas agregar a la lista?(escriba FIN para cancelar)").upper()
+
+if len(mi_lista) > 0:
+    print("Esta es la lista de las compras:")
+
+    for item in mi_lista:
+        print(str(item).capitalize())
+
+    print("La lista ha terminado")
+else:
+    print("Tu lista de compras está vacía")
+
+print("¡Hasta luego!")
